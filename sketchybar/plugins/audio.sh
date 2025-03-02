690a22ca18f6bccefcfcae60153340a9a54b4502
q <qlsen@gmx.net>
2025-03-02 19:40:33 +0100

    2025-03-02 19:40:32

--- sketchybar/plugins/audio.sh
+++ sketchybar/plugins/audio.sh
@@ -6,12 +6,15 @@ if [ "$SENDER" = "volume_change" ]; then
     if [ $INFO = 0 ] ; then
         sketchybar --set audio icon="XX:"
         sketchybar --set audio icon.color=0xffff9500
+        sketchybar --set audio label.color=0xffff9500
     elif [[ $OUTPUT =~ ^(SoundIDReference|BabyfacePro)$ ]]; then 
         sketchybar --set audio icon="AI:"
         sketchybar --set audio icon.color=0xffc7c7c7
+        sketchybar --set audio label.color=0xffc7c7c7
     else
         sketchybar --set audio icon="$(if [ "$INFO" -eq 100 ]; then echo "FF:"; else printf "%02d:" "$INFO"; fi)"
         sketchybar --set audio icon.color=0xffc7c7c7
+        sketchybar --set audio label.color=0xffc7c7c7
     fi
 fi
 sketchybar --set audio label="$OUTPUT"
