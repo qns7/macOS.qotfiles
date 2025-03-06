17c2fe318085cbf83024ed4af63721033cd11db9
qns7 <30077361+qns7@users.noreply.github.com>
2025-03-06 17:41:26 +0100

    2025-03-06 17:41:26

--- sketchybar/items/bar_right_5.sh
+++ sketchybar/items/bar_right_5.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sketchybar --add item bar_right_5 right \
-           --set bar_right_5 icon=":" \
+           --set bar_right_5 icon="_" \
                              icon.color=0xffc7c7c7 \
-                             icon.width=10 \
+                             icon.width=21 \
+                             icon.align=center \
                              icon.y_offset=0
--- sketchybar/plugins/percentages.sh
+++ sketchybar/plugins/percentages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 RAM="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')"
-DISK1="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf ("%s\n", $5) }' | tr -d '%')"
-DISK2="$(df -H | grep -E 'macData' | awk '{print $5}' | tr -d '%')"
+DISK1="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
+DISK2="$(df -H | grep -E 'macData' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
+DISK3="$(df -H | grep -E 'Screenshots' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
 BATTERY="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 
 if [ "$BATTERY" = "" ]; then
@@ -12,7 +13,7 @@ fi
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
 if [[ "$CHARGING" != "" ]]; then
-    sketchybar --set percentages icon="${RAM}R:${DISK1}I.${DISK2}E:${BATTERY}C"
+    sketchybar --set percentages icon="${RAM}R:${DISK1}I.${DISK2}E.${DISK3}S:${BATTERY}C"
 else
-    sketchybar --set percentages icon="${RAM}R:${DISK1}I.${DISK2}E:${BATTERY}B"
+    sketchybar --set percentages icon="${RAM}R:${DISK1}I.${DISK2}E.${DISK3}S:${BATTERY}B"
 fi
