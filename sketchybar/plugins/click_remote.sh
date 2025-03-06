f40bd8b0cea798983b4c177fa4f4d69fdc659225
qns7 <30077361+qns7@users.noreply.github.com>
2025-03-06 13:51:12 +0100

    2025-03-06 13:51:11

--- sketchybar/plugins/click_remote.sh
+++ sketchybar/plugins/click_remote.sh
@@ -4,10 +4,12 @@ STATUS=$(pgrep -x "Remote for Mac")
 
 if [ "$STATUS" = "" ]; then
     sketchybar --set remote icon.color=0xffff9500
-    sleep 0.37
+    sleep 1
     open -a "Remote for Mac"
 else
     sketchybar --set remote icon.color=0xffc7c7c7
+    sleep 1
+    osascript -e 'tell application "Remote for Mac" to quit'
     sleep 0.37
     osascript -e 'tell application "Remote for Mac" to quit'
 fi
