f1e77c0b8697bb3fa7fb601ed97a0b5226b7cc94
q <qlsen@gmx.net>
2025-03-06 17:41:26 +0100

    2025-03-06 17:41:26

--- sketchybar/items/bar_right_1.sh
+++ sketchybar/items/bar_right_1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sketchybar --add item bar_right_1 right \
-           --set bar_right_1 icon="" \
+           --set bar_right_1 icon="_" \
                              icon.color=0xffc7c7c7 \
-                             icon.width=37 \
+                             icon.width=21 \
+                             icon.align=center \
                              icon.y_offset=0
                              # icon=":" \
                              # icon.width=11 \
---
+++ sketchybar/items/sec_dot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sketchybar --add item sec_dot right \
+           --set sec_dot icon="" \
+                         icon.color=0xffc7c7c7 \
+                         icon.width=7 \
+                         icon.y_offset=0
