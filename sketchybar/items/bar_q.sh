68f3a69f7c9aabbae4de23451ce6b3da20c9c924
qns7 <30077361+qns7@users.noreply.github.com>
2025-03-07 19:34:42 +0100

    2025-03-07 19:34:42

--- sketchybar/items/bar_q.sh
+++ sketchybar/items/bar_q.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sketchybar --add item bar_right_1 q \
-           --set bar_right_1 icon=":" \
-                             icon.color=0xffc7c7c7 \
-                             icon.width=11 \
-                             icon.y_offset=0
+sketchybar --add item bar_q q \
+           --set bar_q icon="_" \
+                       icon.color=0xffc7c7c7 \
+                       icon.width=21 \
+                       icon.align=center \
+                       icon.y_offset=0
