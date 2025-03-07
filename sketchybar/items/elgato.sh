68e073aee427fd4399e563eea3f5d6fdeb5efa8b
qns7 <30077361+qns7@users.noreply.github.com>
2025-03-07 17:08:49 +0100

    2025-03-07 17:08:49

--- sketchybar/items/elgato.sh
+++ sketchybar/items/elgato.sh
@@ -3,7 +3,7 @@
 sketchybar --add item elgato right \
            --set elgato script="$PLUGIN_DIR/elgato.sh" \
                         click_script="$PLUGIN_DIR/click_elgato.sh" \
-                        icon="EKL" \
+                        icon="ELG" \
                         icon.font.family="SF Mono" \
                         icon.font.style="Medium" \
                         icon.width=34 \
