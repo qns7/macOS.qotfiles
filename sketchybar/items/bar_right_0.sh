38a30ab554ed767bef1656afc75985f3ef427b08
q <qlsen@gmx.net>
2025-03-07 14:51:10 +0100

    2025-03-07 14:51:10

---
+++ sketchybar/items/bar_right_0.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sketchybar --add item bar_right_0 right \
+           --set bar_right_0 icon="_" \
+                             icon.color=0xffc7c7c7 \
+                             icon.width=21 \
+                             icon.align=center \
+                             icon.y_offset=0
+                             # icon=":" \
+                             # icon.width=11 \
---
+++ sketchybar/items/fn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sketchybar --add item fn right \
+           --set fn script="$PLUGIN_DIR/fn.sh" \
+                    icon="FN" \
+                    icon.font.family="SF Mono" \
+                    icon.font.style="Medium" \
+                    icon.width=23 \
+                    icon.color=0xffc7c7c7 \
+                    icon.y_offset=0
