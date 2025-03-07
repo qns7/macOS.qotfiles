af59f2a982e412df157b8353c282e0f8a902ed4c
qns7 <30077361+qns7@users.noreply.github.com>
2025-03-07 16:33:25 +0100

    2025-03-07 16:33:25

--- sketchybar/items/bar_right_5.sh
+++ sketchybar/items/bar_right_5.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sketchybar --add item bar_right_5 right \
-           --set bar_right_5 icon="_" \
+           --set bar_right_5 icon=":" \
                              icon.color=0xffc7c7c7 \
-                             icon.width=21 \
-                             icon.align=center \
+                             icon.width=10 \
                              icon.y_offset=0
