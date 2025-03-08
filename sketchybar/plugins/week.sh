1ecd253c8d6eb611e5681158e4b4ed85020ea912
qns7 <30077361+qns7@users.noreply.github.com>
2025-03-08 13:52:55 +0100

    2025-03-08 13:52:55

--- sketchybar/plugins/week.sh
+++ sketchybar/plugins/week.sh
@@ -5,8 +5,3 @@ if [ $(yabai -m config menubar_opacity) = 0.0000 ]; then
 else
     sketchybar --set week label.color=0xffff9500
 fi
-    # sketchybar --set week label="􀤁" \
-    #                       label.font.size=17 \
-    #                       label.color=0xffff9500 \
-    #                       label.padding_left=1 \
-    #                       label.y_offset=1
