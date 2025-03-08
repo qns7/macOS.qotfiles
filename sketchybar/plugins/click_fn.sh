dca0fa44fb554085f7566d3dedf44f90a01ef0bc
q <qlsen@gmx.net>
2025-03-08 12:29:48 +0100

    2025-03-08 12:29:48

--- sketchybar/plugins/click_fn.sh
+++ sketchybar/plugins/click_fn.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-open "keysmith://run-macro/92DE10CF-4921-42B9-B165-103AC03D2F57"
+if [ "$(sketchybar --query fn | jq '.icon.color')" = "0xffff9500" ]; then
+    sketchybar --set fn icon.color=0xff4e4e4e
+else
+    sketchybar --set fn icon.color=0xffff9500
+fi
+CURRENT=$(defaults read -g com.apple.keyboard.fnState)
+((CURRENT == 1)) && NEW=false || NEW=true
+defaults write -g com.apple.keyboard.fnState -bool $NEW
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+sleep 0.37
+sketchybar --update
+
+# shortcut and keysmith solutions deprecated, activateSettings is necessary for the change to take effect
+# open "keysmith://run-macro/92DE10CF-4921-42B9-B165-103AC03D2F57"
