d1eb9570c7638c54039667ede25a918739f0072a
q <qlsen@gmx.net>
2025-03-10 09:39:34 +0100

    2025-03-10 09:39:34

--- sketchybar/items/percentages.sh
+++ sketchybar/items/percentages.sh
@@ -8,3 +8,4 @@ sketchybar --add item percentages q \
                              icon.y_offset=0 \
                              update_freq=30 \
            --subscribe percentages system_woke power_source_change
+                             # update_freq=120 ?
