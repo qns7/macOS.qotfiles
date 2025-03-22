04ddabf2ecc55385ace1aa7f643cf3bf31c620a6
q <qlsen@gmx.net>
2025-03-22 11:54:48 +0100

    2025-03-22 11:54:48

--- sketchybar/plugins/click_week.sh
+++ sketchybar/plugins/click_week.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-/Users/q/bin/yabaitoggle "yabai -m config menubar_opacity"
+/Users/q/bin/yabaitoggle "/opt/homebrew/bin/yabai -m config menubar_opacity"
 /Users/q/.config/sketchybar/plugins/week.sh
+# in linearmouse for debugging used: /bin/bash /Users/q/.config/sketchybar/plugins/click_week.sh > /tmp/lm_debug.log 2>&1
+# full paths necessary in every script involoved
--- sketchybar/plugins/week.sh
+++ sketchybar/plugins/week.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ $(yabai -m config menubar_opacity) = 0.0000 ]; then
-    sketchybar --set week label.color=0xffc7c7c7
+if [ $(/opt/homebrew/bin/yabai -m config menubar_opacity) = 0.0000 ]; then
+    /opt/homebrew/bin/sketchybar --set week label.color=0xffc7c7c7
 else
-    sketchybar --set week label.color=0xffff9500
+    /opt/homebrew/bin/sketchybar --set week label.color=0xffff9500
 fi
+# in linearmouse for debugging used: /bin/bash /Users/q/.config/sketchybar/plugins/click_week.sh > /tmp/lm_debug.log 2>&1
+# full paths necessary in every script involoved
