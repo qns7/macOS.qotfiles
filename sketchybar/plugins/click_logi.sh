b9aeacf68beaf2e6dd76b2f8481b91447cb07951
qns7 <30077361+qns7@users.noreply.github.com>
2025-03-30 10:54:21 +0200

    2025-03-30 10:54:21

--- sketchybar/plugins/click_logi.sh
+++ sketchybar/plugins/click_logi.sh
@@ -3,10 +3,6 @@
 STATUS=$(pgrep -x "LogiTune")
 
 sketchybar --set logi icon.color=0xffff9500
-# yabai -m config mouse_follows_focus on # alttab
 sleep 0.1
 open -a "Logi Tune"
-# skhd -k "fn - x"
-# yabai -m config mouse_follows_focus off # alttab
-# doesn't work because LogiTune has no normal window
 /Users/q/.config/sketchybar/plugins/logi.sh
