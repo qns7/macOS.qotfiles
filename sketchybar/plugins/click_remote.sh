cf44641221fdfe50e720ebf7d8c7c14c0eb5d111
qns7 <30077361+qns7@users.noreply.github.com>
2025-04-09 11:50:03 +0200

    2025-04-09 11:50:03

--- sketchybar/plugins/click_remote.sh
+++ sketchybar/plugins/click_remote.sh
@@ -9,7 +9,11 @@ if [ "$STATUS" = "" ]; then
 else
     sketchybar --set remote icon.color=0xff4e4e4e
     sleep 1
-    osascript -e 'tell application "Remote for Mac" to quit'
+    # osascript -e 'tell application "Remote for Mac" to quit'
+    # killall "cewd"
+    # sleep 1
+    # killall "Remote for Mac"
+    pkill -9 -f "Remote for Mac"
     sleep 1
     /Users/q/.config/sketchybar/plugins/remote.sh
 fi
