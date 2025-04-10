4a08b9df59bd67a37ab6693d8aa06aa3f43e1b47
qns7 <30077361+qns7@users.noreply.github.com>
2025-04-10 21:44:05 +0200

    2025-04-10 21:44:05

--- sketchybar/items/space_info.sh
+++ sketchybar/items/space_info.sh
@@ -8,4 +8,4 @@ sketchybar --add item space_info left \
                             label.color=0xffc7c7c7 \
                             label.padding_left=10 \
                             label.width=102 \
-           --subscribe space_info display_change space_change
+           --subscribe space_info display_change space_change # space_windows_change via yabai
