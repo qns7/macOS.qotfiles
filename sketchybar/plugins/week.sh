e91dbe298441d44f632c6b1a2a7ba81bdd507b12
qns7 <30077361+qns7@users.noreply.github.com>
2025-04-25 17:05:49 +0200

    2025-04-25 17:05:49

--- sketchybar/plugins/week.sh
+++ sketchybar/plugins/week.sh
@@ -4,6 +4,7 @@ if [ $(/opt/homebrew/bin/yabai -m config menubar_opacity) = 0.0000 ]; then
     /opt/homebrew/bin/sketchybar --set week label.color=0xffc7c7c7    
 else
     /opt/homebrew/bin/sketchybar --set week label.color=0xffff9500
+fi
     # focused_display_index=$(/opt/homebrew/bin/yabai -m query --displays --display | jq -r '.index')
     # display_info=$(/opt/homebrew/bin/yabai -m query --displays | /opt/homebrew/bin/jq -r --arg index "$focused_display_index" '.[] | select(.index == ($index | tonumber))')
     # x=$(echo "$display_info" | /opt/homebrew/bin/jq -r '.frame.x')
@@ -12,4 +13,3 @@ else
     # target_x=$(printf "%.0f" "$(echo "$x + $w - 37" | bc)")
     # target_y=$(printf "%.0f" "$y")
     # /opt/homebrew/bin/cliclick "m:$target_x,$target_y"
-fi
