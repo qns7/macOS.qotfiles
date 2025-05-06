fc980dd16dae234d19ba75a37f3489017831168c
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-06 19:18:28 +0200

    2025-05-06 19:18:28

--- sketchybar/plugins/percentages.sh
+++ sketchybar/plugins/percentages.sh
@@ -2,7 +2,10 @@
 
 SWAP="$(sysctl vm.swapusage | awk '{print $7}' | sed 's/M//' | awk '{gb = $1 / 1024; printf "%02d\n", (gb*10 == int(gb*10)) ? gb*10 : int(gb*10) + 1}')"
 RAM="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')"
-DISK1="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
+# DISK1="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
+# DISK1="$(df -H | grep -E '^(/dev/disk1s5).' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
+# DISK1="$(df -H | grep -E '/System/Volumes/Data' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
+DISK1="$(df -H | grep -E '^/dev/.* /System/Volumes/Data$' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
 DISK2="$(df -H | grep -E 'macData' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%' )"
 DISK2="${DISK2:-XX}"
 DISK3="$(df -H | grep -E 'Screenshots' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%' )"
