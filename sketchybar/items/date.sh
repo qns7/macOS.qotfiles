4ce33db5e0bbde82779d4aec09a28a4c50a2b04f
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-11 11:42:45 +0200

    2025-05-11 11:42:45

--- sketchybar/items/date.sh
+++ sketchybar/items/date.sh
@@ -7,4 +7,4 @@ sketchybar --add item date right \
                       icon.y_offset=0 \
                       label.color=0xffc7c7c7 \
                       label.width=22 \
-                      update_freq=120
+                      update_freq=120 # safe? nach wake wars gereade noch die alte nummer - why? wäre es nach 120 sec nice? 11:36, 11.05.25, SUN
