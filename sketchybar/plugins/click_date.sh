9fb7dd3b4ffcf91d93371fe0b31f52cd04b8d750
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-11 21:23:52 +0200

    2025-05-11 21:23:52

--- sketchybar/plugins/click_date.sh
+++ sketchybar/plugins/click_date.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-CLOCK="$HOME/.config/sketchybar/clock"
-PID=$(pgrep -f "$CLOCK")
-if [ -z "$PID" ]; then
-  sketchybar --set date icon=""
-  "$CLOCK" &
-else
-  kill "$PID"
-fi
-~/.config/sketchybar/plugins/date.sh
-
-# if [ -f /tmp/show.clock ]; then
-#     rm /tmp/show.clock
+# CLOCK="$HOME/.config/sketchybar/clock"
+# PID=$(pgrep -f "$CLOCK")
+# if [ -z "$PID" ]; then
+#   sketchybar --set date icon=""
+#   "$CLOCK" &
 # else
-#     touch /tmp/show.clock
+#   kill "$PID"
 # fi
-# /Users/q/.config/sketchybar/plugins/date.sh
+# ~/.config/sketchybar/plugins/date.sh
+
+if [ -f /tmp/show.clock ]; then
+    rm /tmp/show.clock
+else
+    touch /tmp/show.clock
+fi
+/Users/q/.config/sketchybar/plugins/date.sh
