f60d2439b0d849472b11d6349bc88c361ad54d75
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-13 18:25:20 +0200

    2025-05-13 18:25:20

--- sketchybar/items/date.sh
+++ sketchybar/items/date.sh
@@ -3,9 +3,9 @@
 sketchybar --add item date right \
            --set date script="$PLUGIN_DIR/date.sh"  \
                       click_script="$PLUGIN_DIR/click_date.sh" \
-                      icon.width=91 \
+                      icon.width=57 \
                       icon.y_offset=0 \
                       label.width=22 \
                       label.color=0xffc7c7c7 \
                       update_freq=120
-                      # icon.width=113 \
+                      # icon.width=113=91+22|w/o_year_57+22=79
--- sketchybar/plugins/date.sh
+++ sketchybar/plugins/date.sh
@@ -1,11 +1,11 @@
 # #!/bin/bash
 
 if [ -f /tmp/show.clock ]; then
-    sketchybar --set date icon="$(date +'%H:%M:%S')" \
+    sketchybar --set date icon="$(date +'%H:%M')" \
                           icon.font.style="Medium" \
                           icon.color=0xffffffff
 else
-    sketchybar --set date icon="$(date +'%d.%m.%y')" \
+    sketchybar --set date icon="$(date +'%d.%m')" \
                           icon.font.style="Regular" \
                           icon.color=0xffc7c7c7
 fi
