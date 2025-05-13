f2f2abc1cf476b47954353f612194f9e3789c776
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-13 11:04:31 +0200

    2025-05-13 11:04:31

--- sketchybar/plugins/date.sh
+++ sketchybar/plugins/date.sh
@@ -1,7 +1,7 @@
 # #!/bin/bash
 
 if [ -f /tmp/show.clock ]; then
-    sketchybar --set date icon="$(date +'%H:%M___:%u')"
+    sketchybar --set date icon="$(date +'%Huhr:%M:%u')"
 else
     sketchybar --set date icon="$(date +'%d.%m.%y:%u')"
 fi
