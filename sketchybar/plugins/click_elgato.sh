857d60e8777141a731d85045172b80368335fe85
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-25 21:20:47 +0200

    2025-05-25 21:20:47

--- sketchybar/plugins/click_elgato.sh
+++ sketchybar/plugins/click_elgato.sh
@@ -6,6 +6,9 @@ if [ "$STATUS" = "" ]; then
     sketchybar --set elgato icon.color=0xffff9500
     sleep 0.37
     open -a "Elgato Control Center"
+    sleep 1
+    open_or_focus "Elgato Control Center"
+    skhd -k "ctrl - down"
 else
     sketchybar --set elgato icon.color=0xff4e4e4e
     sleep 0.37
