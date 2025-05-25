143318e1cdb9774671ea563c56833fb9bd0926b4
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-25 21:46:30 +0200

    2025-05-25 21:46:30

--- sketchybar/plugins/click_elgato.sh
+++ sketchybar/plugins/click_elgato.sh
@@ -4,10 +4,16 @@ STATUS=$(pgrep -x "Elgato Control Center")
 
 if [ "$STATUS" = "" ]; then
     sketchybar --set elgato icon.color=0xffff9500
-    sleep 0.37
+    # sleep 0.37
     open -a "Elgato Control Center"
+    sleep 0.1
+    open -a "Elgato Control Center"
+    sleep 0.1
+    skhd -k "ctrl - down"
 else
     sketchybar --set elgato icon.color=0xff4e4e4e
-    sleep 0.37
-    osascript -e 'tell application "Elgato Control Center" to quit'
+    killall "Elgato Control Center" &> /dev/null
+    # sleep 0.37
+    # osascript -e 'tell application "Elgato Control Center" to quit'
 fi
+/Users/q/.config/sketchybar/plugins/elgato.sh
--- sketchybar/plugins/click_logi.sh
+++ sketchybar/plugins/click_logi.sh
@@ -4,6 +4,6 @@
 # 
 sketchybar --set logi icon.color=0xffff9500
 sleep 0.1
-open -a "Logi Tune"
+! pgrep -x "LogiTune" > /dev/null && (open -a "Logi Tune" && sleep 1 && skhd -k "ctrl - down") || open -a "Logi Tune"
 sleep 1
 /Users/q/.config/sketchybar/plugins/logi.sh
