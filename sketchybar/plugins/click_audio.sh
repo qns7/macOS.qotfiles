d3f8ec817e2cc2e8ba1f9db1f5ea454edfe18264
qns7 <30077361+qns7@users.noreply.github.com>
2025-05-29 11:32:20 +0200

    2025-05-29 11:32:20

--- sketchybar/plugins/click_audio.sh
+++ sketchybar/plugins/click_audio.sh
@@ -2,8 +2,8 @@
 
 if [[ $(yabai -m query --windows --window | jq -r '.app') != "SoundID Reference" ]]; then
     /Users/q/bin/open_or_focus "SoundID Reference"
-    sleep 0.37
-    cliclick $(yabai -m query --windows --window | jq -r '.frame | "m:\(.x + (.w/2) | floor),\(.y + (.h/2) | floor)"')
+    # sleep 0.37
+    # cliclick $(yabai -m query --windows --window | jq -r '.frame | "m:\(.x + (.w/2) | floor),\(.y + (.h/2) | floor)"')
 else
     ~/bin/close_soundid
 fi
