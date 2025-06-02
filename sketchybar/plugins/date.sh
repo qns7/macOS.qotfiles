5552c6f067300f6a43f9df471e80552a48b6e937
qns7 <30077361+qns7@users.noreply.github.com>
2025-06-02 21:34:18 +0200

    2025-06-02 21:34:18

--- sketchybar/plugins/date.sh
+++ sketchybar/plugins/date.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+#!/bin/bash
 
 if [ -f /tmp/show.clock ]; then
     sketchybar --set date icon="$(date +'%H:%M')" \
--- sketchybar/plugins/week.sh
+++ sketchybar/plugins/week.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sketchybar --set week label="$(date +'%V')"
-if [ $(/opt/homebrew/bin/yabai -m config menubar_opacity) = 0.0000 ]; then
+if [ "$(/opt/homebrew/bin/yabai -m config menubar_opacity)" = 0.0000 ]; then
     /opt/homebrew/bin/sketchybar --set week label.color=0xffc7c7c7    
 else
     /opt/homebrew/bin/sketchybar --set week label.color=0xffff9500
