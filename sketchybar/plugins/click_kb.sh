f2f0cdf9069645171f22d80fc662e65505b27124
qns7 <30077361+qns7@users.noreply.github.com>
2025-08-03 10:44:27 +0200

    2025-08-03 10:44:27

--- sketchybar/plugins/click_kb.sh
+++ sketchybar/plugins/click_kb.sh
@@ -5,9 +5,13 @@ STATUS=$(pgrep -x KeyboardCleanTool)
 if [ "$STATUS" = "" ]; then
     sketchybar --set kb icon.color=0xffff9500
     sleep 0.37
+    killall mouseless
+    '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile 'OFF'
     open -a "KeyboardCleanTool"
 else
     sketchybar --set kb icon.color=0xff4e4e4e
     sleep 0.37
     osascript -e 'tell application "KeyboardCleanTool" to quit'
+    '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile 'q'
+    open -a "mouseless"
 fi
