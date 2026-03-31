3c34ddac26b27c96f05dd547c60524b2c3ad7370
qns7 <30077361+qns7@users.noreply.github.com>
2026-03-31 21:56:28 +0200

    2026-03-31 21:56:28

--- sketchybar/items/sec_dot.sh
+++ sketchybar/items/sec_dot.sh
@@ -2,6 +2,7 @@
 
 sketchybar --add item sec_dot right \
            --set sec_dot icon="" \
+                         click_script="$PLUGIN_DIR/click_sec_dot.sh" \
                          icon.width=7 \
                          icon.y_offset=0 \
                          updates=off
---
+++ sketchybar/plugins/click_sec_dot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/Users/q/bin/yabaitoggle "/opt/homebrew/bin/yabai -m config menubar_opacity"
+/Users/q/.config/sketchybar/plugins/week.sh
