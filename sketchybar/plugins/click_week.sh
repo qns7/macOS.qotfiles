1921a520f38bbe476f11655c0f55ea5312b4e112
qns7 <30077361+qns7@users.noreply.github.com>
2026-04-03 10:51:17 +0200

    2026-04-03 10:51:17

--- sketchybar/plugins/click_week.sh
+++ sketchybar/plugins/click_week.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+CURRENT_COLOR=$(/opt/homebrew/bin/sketchybar --query week | jq -r '.label.color')
+if [ "$CURRENT_COLOR" = "0xffc7c7c7" ]; then
+    /opt/homebrew/bin/sketchybar --set week label.color=0xffff9500
+else
+    /opt/homebrew/bin/sketchybar --set week label.color=0xffc7c7c7
+fi
 /Users/q/bin/yabaitoggle "/opt/homebrew/bin/yabai -m config menubar_opacity"
 /Users/q/.config/sketchybar/plugins/week.sh
