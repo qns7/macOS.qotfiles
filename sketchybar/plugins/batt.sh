920af51e83903764855c32440efecd28bd8104c2
qns7 <30077361+qns7@users.noreply.github.com>
2026-04-25 01:21:13 +0200

    2026-04-25 01:21:13

--- sketchybar/plugins/batt.sh
+++ sketchybar/plugins/batt.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# SWAP="$(sysctl vm.swapusage | awk '{print $7}' | sed 's/M//' | awk '{gb = $1 / 1024; printf "%02d\n", (gb*10 == int(gb*10)) ? gb*10 : int(gb*10) + 1}')"
-# RAM="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')"
-
-# DISK1="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
-# DISK1="$(df -H | grep -E '^(/dev/disk1s5).' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
-# DISK1="$(df -H | grep -E '/System/Volumes/Data' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
-DISK1="$(df -H | grep -E '^/dev/.* /System/Volumes/Data$' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%')"
-DISK2="$(df -H | grep -E 'macData' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%' )"
-DISK2="${DISK2:-XX}"
-DISK3="$(df -H | grep -E 'Screenshots' | awk '{ printf("%02.0f\n", $5) }' | tr -d '%' )"
-DISK3="${DISK3:-XX}"
 BATTERY="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 [ "$BATTERY" -eq 100 ] && BATTERY="FF"
-# the others won't get to 100, so no worries
 
 if [ "$BATTERY" = "" ]; then
     exit 0
@@ -21,22 +9,8 @@ fi
 
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
-if pgrep -xq "Disk Utility"; then
-    ICON_COLOR="icon.color=0xff4e4e4e"
-else
-    ICON_COLOR="icon.color=0xffc7c7c7"
-fi
-
 if [[ "$CHARGING" != "" ]]; then
-    sketchybar --set percentages $ICON_COLOR icon="$:${DISK1}I.${DISK2}E.${DISK3}S:${BATTERY}C"
+    sketchybar --set batt $ICON_COLOR icon="${BATTERY}C"
 else
-    sketchybar --set percentages $ICON_COLOR icon="$:${DISK1}I.${DISK2}E.${DISK3}S:${BATTERY}B"
+    sketchybar --set batt $ICON_COLOR icon="${BATTERY}B"
 fi
-
-# if [[ "$CHARGING" != "" ]]; then
-#     sketchybar --set percentages icon="${SWAP}S.${RAM}R:${DISK1}I.${DISK2}E.${DISK3}S:${BATTERY}C"
-# else
-#     sketchybar --set percentages icon="${SWAP}S.${RAM}R:${DISK1}I.${DISK2}E.${DISK3}S:${BATTERY}B"
-# fi
-
-# DO IT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
