dee3e2f08cfb7aaff9976f9aa7e9bee0b0e8fabe
qns7 <30077361+qns7@users.noreply.github.com>
2026-04-27 09:51:04 +0200

    2026-04-27 09:51:04

--- sketchybar/plugins/click_kb.sh
+++ sketchybar/plugins/click_kb.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-STATUS=$(pgrep -x KeyboardCleanTool)
+KARABINER="/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
+PID_FILE="/tmp/keyboard_block.pid"
 
-if [ "$STATUS" = "" ]; then
-    sketchybar --set kb icon.color=0xffff9500
-    sleep 0.37
-    killall mouseless
-    '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile 'OFF'
-    open -a "KeyboardCleanTool"
-else
+if [ -f "$PID_FILE" ]; then
+    kill "$(cat "$PID_FILE")" 2>/dev/null
+    rm "$PID_FILE"
     sketchybar --set kb icon.color=0xff4e4e4e
-    sleep 0.37
-    osascript -e 'tell application "KeyboardCleanTool" to quit'
-    '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile 'q'
+    "$KARABINER" --select-profile 'q'
     open -a "mouseless"
+else
+    sketchybar --set kb icon.color=0xffff9500
+    killall mouseless
+    "$KARABINER" --select-profile 'OFF'
+    /Users/q/Developer/APPS/keyboardcleaner/keyboardcleaner &
+    echo $! > "$PID_FILE"
 fi
+
+# #!/bin/bash
+
+# STATUS=$(pgrep -x KeyboardCleanTool)
+
+# if [ "$STATUS" = "" ]; then
+#     sketchybar --set kb icon.color=0xffff9500
+#     sleep 0.37
+#     killall mouseless
+#     '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile 'OFF'
+#     open -a "KeyboardCleanTool"
+# else
+#     sketchybar --set kb icon.color=0xff4e4e4e
+#     sleep 0.37
+#     osascript -e 'tell application "KeyboardCleanTool" to quit'
+#     '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --select-profile 'q'
+#     open -a "mouseless"
+# fi
