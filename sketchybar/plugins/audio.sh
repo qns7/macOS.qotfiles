4e28449842a29412c26bb1877acfc27528d09ad8
qns7 <30077361+qns7@users.noreply.github.com>
2026-07-27 21:49:12 +0200

    2026-07-27 21:49:12

--- sketchybar/plugins/audio.sh
+++ sketchybar/plugins/audio.sh
@@ -3,18 +3,43 @@
 OUTPUT="$(SwitchAudioSource -c | tr -cd '[:alpha:]')"
 
 if [ "$SENDER" = "volume_change" ]; then
-    if [ $INFO = 0 ] ; then
+    if [ "$INFO" = 0 ]; then
         sketchybar --set audio icon="XX:"
         sketchybar --set audio icon.color=0xff4e4e4e
         sketchybar --set audio label.color=0xff4e4e4e
-    elif [[ $OUTPUT =~ ^(SoundIDReference|BabyfacePro)$ ]]; then 
+    elif [[ $OUTPUT =~ ^(SoundIDReference|BabyfacePro)$ ]]; then
         sketchybar --set audio icon="AI:"
         sketchybar --set audio icon.color=0xffc7c7c7
         sketchybar --set audio label.color=0xffc7c7c7
+    elif [ "$INFO" -eq 100 ]; then
+        sketchybar --set audio icon="FF:"
+        sketchybar --set audio icon.color=0xFFFC5753
+        sketchybar --set audio label.color=0xFFFC5753
     else
-        sketchybar --set audio icon="$(if [ "$INFO" -eq 100 ]; then echo "FF:"; else printf "%02d:" "$INFO"; fi)"
+        sketchybar --set audio icon="$(printf "%02d:" "$INFO")"
         sketchybar --set audio icon.color=0xffc7c7c7
         sketchybar --set audio label.color=0xffc7c7c7
     fi
 fi
 sketchybar --set audio label="$OUTPUT"
+
+# #!/bin/bash
+
+# OUTPUT="$(SwitchAudioSource -c | tr -cd '[:alpha:]')"
+
+# if [ "$SENDER" = "volume_change" ]; then
+#     if [ $INFO = 0 ] ; then
+#         sketchybar --set audio icon="XX:"
+#         sketchybar --set audio icon.color=0xff4e4e4e
+#         sketchybar --set audio label.color=0xff4e4e4e
+#     elif [[ $OUTPUT =~ ^(SoundIDReference|BabyfacePro)$ ]]; then 
+#         sketchybar --set audio icon="AI:"
+#         sketchybar --set audio icon.color=0xffc7c7c7
+#         sketchybar --set audio label.color=0xffc7c7c7
+#     else
+#         sketchybar --set audio icon="$(if [ "$INFO" -eq 100 ]; then echo "FF:"; else printf "%02d:" "$INFO"; fi)"
+#         sketchybar --set audio icon.color=0xffc7c7c7
+#         sketchybar --set audio label.color=0xffc7c7c7
+#     fi
+# fi
+# sketchybar --set audio label="$OUTPUT"
